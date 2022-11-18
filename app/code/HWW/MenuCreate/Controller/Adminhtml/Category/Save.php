0112ec7b8c65635f27a5e6e7e4a93aefcc5c3983
alhussain50 <alhussain@iut-dhaka.edu>
2022-11-18 18:53:53 +0600

    grid gets updated after saving form

---
+++ app/code/HWW/MenuCreate/Controller/Adminhtml/Category/Save.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace HWW\MenuCreate\Controller\Adminhtml\Category;
+
+use HWW\MenuCreate\Model\Category as ItemFactory;
+
+class Save extends \Magento\Backend\App\Action
+{
+    private $itemFactory;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        ItemFactory $itemFactory
+    ) {
+        $this->itemFactory = $itemFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+// var_dump($this->getRequest()->getParams());exit;
+        // $this->itemFactory->create()
+        //     ->setData($this->getRequest()->getParams('general'))
+        //     ->save();
+        // return $this->resultRedirectFactory->create()->setPath('menucreate/create/index');
+        $this->itemFactory
+            ->setData($this->getRequest()->getParam('general'))
+            ->save();
+        return $this->resultRedirectFactory->create()->setPath('menucreate/category/index');
+    }
+}
---
+++ app/code/HWW/MenuCreate/Ui/Item/DataProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace HWW\MenuCreate\Ui\Item;
+
+use HWW\MenuCreate\Model\ResourceModel\Item\Collection;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+
+class DataProvider extends AbstractDataProvider
+{
+    protected $collection;
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        Collection $collectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+        // var_dump(is_array($collectionFactory->create()));exit;
+    }
+    
+    public function addFilter(\Magento\Framework\Api\Filter $filter)
+    {
+        return null;
+    }
+
+    public function getData()
+    {
+        $result = [];
+        // var_dump($this->collection);exit;
+        // foreach ($this->collection->getItems() as $item) {
+        //     $result[$item->getId()]['general'] = $item->getData();
+        // }
+        return $result;
+    }
+}
